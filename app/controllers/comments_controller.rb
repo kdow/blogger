4eab81019aa70b6b45bf1e33a2669b1d6172de84
Kelly Downes <kedownes@gmail.com>
2015-12-13 21:02:17 -0800

    Sorcery authentication complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_filter :require_login, except: [:create]
 
   def create
     @comment = Comment.new(comment_params)
@@ -7,6 +8,13 @@ class CommentsController < ApplicationController
     @comment.save
 
     redirect_to article_path(@comment.article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   def comment_params
